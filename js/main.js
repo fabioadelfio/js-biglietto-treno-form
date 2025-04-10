5180573c63dedbf16d938b0ec7fca4168dee78ab
fabioadelfio <adelfiofabio07@gmail.com>
2025-04-10 16:57:16 +0200

    form

--- js/main.js
+++ js/main.js
@@ -13,10 +13,21 @@ ticketForm.addEventListener(`submit`, function (event) {
     event.preventDefault();
 
     const fullname = fullnameInput.value;
-    const km = kmInput.value;
+    const km = parseInt(kmInput.value);
     const ageGroup = ageGroupInput.value;
-
     
+    const myTicket = document.getElementById(`my-ticket`);
+    const passengerName = document.getElementById(`passenger-name`);
+    const offerType = document.getElementById(`offer-type`);
+    const ticketPrice = document.getElementById(`ticket-price`);
+
+    myTicket.classList.remove(`d-none`);
+    myTicket.classList.remove(`d-block`);
+    
+    if(!fullname) return;
+    if(isNaN(km)) return;
+    if(!ageGroup) return;
+
     const price = 0.21 * km;
     let discountPerc = ``;
     let discountAmount = ``;
@@ -26,20 +37,26 @@ ticketForm.addEventListener(`submit`, function (event) {
         discountPerc = 20;
         discountAmount = price * discountPerc / 100;
         finalPrice = price - discountAmount;
+        offerType.append(`Biglietto Minorenne`);
     } 
     else if (ageGroup === `Over 65`) {
         discountPerc = 40;
         discountAmount = price * discountPerc / 100;
         finalPrice = price - discountAmount;
+        offerType.append(`Biglietto Over 65`);
     }
     else if (ageGroup === `Maggiorenne`){
         finalPrice = price;
+        offerType.append(`Biglietto Standard`);
     }
     
     console.log(`Biglietto:`);
     console.log(`Nome e cognome: ${fullname}`);
     console.log(`Km da percorrere ${km}`);
     console.log(`Fascia d'età: ${ageGroup}`);
-    console.log(`Il prezzo del biglietto è: ${finalPrice.toFixed(2)}`);
+    console.log(`Il prezzo del biglietto è: ${finalPrice.toFixed(2)} €`);
+
+    passengerName.append(fullname);
+    ticketPrice.append(`${finalPrice.toFixed(2)} €`);
 })
 
